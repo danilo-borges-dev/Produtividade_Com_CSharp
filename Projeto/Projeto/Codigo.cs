ae626f684bda43d7fcb769adab61a0fcc47c0a79
danilo borges <danilosilva2017@hotmail.com>
2022-06-30 14:38:46 -0300

    Iniciando um novo projeto

---
+++ Projeto/Projeto/Codigo.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello World!");
+
+DateTime hoje = DateTime.Now;
+DateTime ontem = hoje.AddDays(-1);
+DateTime amanha = hoje.AddDays(1);
+Console.WriteLine($"Ontem: {ontem:dd/MM/yyyy}");
+Console.WriteLine($"Hoje: {hoje:dd/MM/yyyy}");
+Console.WriteLine($"Amanhã: {amanha:dd/MM/yyyy}");
