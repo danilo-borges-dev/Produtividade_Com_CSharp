9aa5b1759ed6915bda28669710a2109f56315820
danilo borges <danilosilva2017@hotmail.com>
2022-06-30 14:53:52 -0300

    Convertendo uma string em array de bytes

---
+++ Projeto/Trablhando_Com_Strings/Program.cs
@@ -0,0 +1,7 @@
+﻿// A forma recomendada de trabalhar com String atualmente é:
+
+var mensage = @"O sucesso é diário, 
+nas pequenas coisas e treinárvel!";
+// Acima temos a forma ideal de trabalhar com quebra de linha e tabulação
+
+Console.WriteLine($"{mensage}");
